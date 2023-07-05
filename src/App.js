87e55dff28a7b9597ebf7b0aae8e52ac7778cdb8
mrpkm <mrpkdmk01@gmail.com>
2023-07-05 20:58:49 +0530

    adding chat-room project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
+import { useEffect, useState } from "react";
 import './App.css';
+import firebase from './firebase';
+import { getDatabase, push, ref, set, onChildAdded } from 'firebase/database'
 
 function App() {
+  const [name, setName] = useState("");
+  const [chats, setChats] = useState([]);
+  const [msg, setMsg] = useState("");
+
+  const db = getDatabase();
+  const chatListRef = ref(db, "chats");
+
+  const handleInputChange = () => {
+    const inputName = prompt('Please enter your name:');
+    setName(inputName);
+  };
+
+
+  useEffect(() => {
+    onChildAdded(chatListRef, (data) => {
+      // const c = [...chats];
+      // c.push(data.val());
+      // setChats(c)
+      setChats(chats => [...chats, data.val()])
+      console.log(data.val());
+    });
+  }, [])
+
+  const sendChat = () => {
+    const chatRef = push(chatListRef);
+    set(chatRef, {
+      name, message: msg
+    })
+    setMsg("");
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="app">
+      {name ? null : <div className="setnamebtn">
+        <button className="setbtn" onClick={handleInputChange}>Set Name</button>
+      </div>}
+      {name ?
+        <div>
+          <h1>user: {name}</h1>
+          <div className="chat-container">
+            {chats.map((c, i) => <div key={i} className={`container ${c.name === name ? 'me' : ''}`}>
+              <p className="chatbox">
+                <strong>{c.message}</strong>
+                <span>{c.name}</span>
+              </p>
+            </div>)}
+
+          </div>
+          <div className="botton">
+            <input type="text" required onInput={e => setMsg(e.target.value)} placeholder="enter you chat.." value={msg} />
+            <button onClick={e => sendChat()}>send</button>
+          </div>
+        </div> : null}
+
+    </div >
   );
 }
 
---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCNCmHnfWKWNuozk8-P4eXDMTjnIgCqFfY",
+  authDomain: "chat-room-c0489.firebaseapp.com",
+  databaseURL: "https://chat-room-c0489-default-rtdb.firebaseio.com",
+  projectId: "chat-room-c0489",
+  storageBucket: "chat-room-c0489.appspot.com",
+  messagingSenderId: "879135809923",
+  appId: "1:879135809923:web:316bf8127369f33aec96b6"
+};
+
+const app = initializeApp(firebaseConfig);
